20adc3021fa57d5e15145f842e247bf8aaf7d272
nowfox <nowfox@163.com>
2020-03-12 12:17:55 +0800

    补充kube-apiserver

--- mirror/fileRepository/mirror-zhy.sh
+++ mirror/fileRepository/mirror-zhy.sh
@@ -2,7 +2,7 @@
 
 KOPS_VERSION='1.16.0'
 # K8s recommended version for Kops: https://github.com/kubernetes/kops/blob/master/channels/stable
-K8S_RECOMMENDED_VERSION='1.16.7'
+K8S_RECOMMENDED_VERSION='${1-1.16.7}'
 
 KUBERNETES_ASSETS=(
   release/v${K8S_RECOMMENDED_VERSION}/
@@ -17,12 +17,13 @@ mirror_kubernetes_release(){
         fi
         echo gsutil rsync -d -r gs://kubernetes-release/$asset ./kubernetes-release/$asset
         gsutil -m rsync -d -r \
-	      -x ".*s390x|.*ppc64le|.*-arm" \
+	      -x ".*s390x|.*ppc64le|.*windows|.*-arm|.*arm" \
 	      gs://kubernetes-release/$asset ./kubernetes-release/$asset
     done
 }
 
 sync_release_to_s3(){
+    echo "start sync_release_to_s3()"
     aws --profile=zhy s3 sync ./kubernetes-release/ s3://kops-kubernetes-release/ --acl public-read
 }
 
@@ -35,6 +36,7 @@ mirror_kops-kubeupv2(){
         fi
         wget -c https://kubeupv2.s3.amazonaws.com/kops/$KOPS_VERSION/$platform/amd64/kops -O $TMPDIR/$platform/amd64/kops
         wget -c https://kubeupv2.s3.amazonaws.com/kops/$KOPS_VERSION/$platform/amd64/kops.sha1 -O $TMPDIR/$platform/amd64/kops.sha1
+		wget -c https://kubeupv2.s3.amazonaws.com/kops/$KOPS_VERSION/$platform/amd64/kops.sha256 -O $TMPDIR/$platform/amd64/kops.sha256
     done
 
 
@@ -42,6 +44,7 @@ mirror_kops-kubeupv2(){
     do
         wget -c https://kubeupv2.s3.amazonaws.com/kops/$KOPS_VERSION/linux/amd64/$file -O $TMPDIR/linux/amd64/$file
         wget -c https://kubeupv2.s3.amazonaws.com/kops/$KOPS_VERSION/linux/amd64/$file.sha1 -O $TMPDIR/linux/amd64/$file.sha1
+		wget -c https://kubeupv2.s3.amazonaws.com/kops/$KOPS_VERSION/linux/amd64/$file.sha256 -O $TMPDIR/linux/amd64/$file.sha256
     done
 
     if [[ ! -d $TMPDIR/images ]]; then
@@ -57,9 +60,10 @@ mirror_kops-kubeupv2(){
 
 
 mirror_fileRepo(){
-    aws --profile zhy s3 sync s3://kops-kubernetes-release/ s3://kops-file/fileRepository/kubernetes-release/ --acl public-read
+    //在下面这行，s3://kops-file/fileRepository/kops-kubernetes-release/的kops-kubernetes-release必须和前面s3://kops-kubernetes-release/保持一致
+    aws --profile zhy s3 sync s3://kops-kubernetes-release/ s3://kops-file/fileRepository/kops-kubernetes-release/ --acl public-read
     aws --profile zhy s3 sync s3://kops-kubeupv2/ s3://kops-file/fileRepository/kubeupv2/ --acl=public-read
-    aws --profile zhy s3 sync s3://kops-kubeupv2/kops/$KOPS_VERSION s3://kops-file/fileRepository/kops/$KOPS_VERSION --acl public-read 
+    aws --profile zhy s3 sync s3://kops-kubeupv2/kops/$KOPS_VERSION s3://kops-file/fileRepository/kops/$KOPS_VERSION --acl public-read
 }
 
 mirror_kubernetes_release
